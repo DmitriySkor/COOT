e07ef2a92980c5ff07bc9b11ad62aafadc9b59cb
ValentinGor <v.gorbatenko@mail.ru>
2017-12-13 10:50:44 +0200

    Form and map added

--- contact.php
+++ contact.php
@@ -2,10 +2,58 @@
     <div class="contact_cover">
         <div class="container">
             <div class="row">
-                <div class="col-md-12">
-
+                <div class="col-md-6"></div>
+                <div class="col-md-6">
+                    <h2>Контакты</h2>
+                    <div class="contact_info_cover">
+                        <div class="contact_info_name">Телефон:</div>
+                        <div class="contact_info_text">+7(4852) 73-72-68<br>+7(4852) 73-86-58</div>
+                    </div>
+                    <div class="contact_info_cover">
+                        <div class="contact_info_name">Адрес:</div>
+                        <div class="contact_info_text">г. Ярославль, пр-т Октября, <br>д. 88, офис 102, пом. 2.</div>
+                    </div>
+                    <div class="contact_info_cover">
+                        <div class="contact_info_name">E-mail:</div>
+                        <div class="contact_info_text">info@оибт.рф</div>
+                    </div>
+                    <div class="contact_info_cover">
+                        <div class="contact_info_name">Сайт:</div>
+                        <div class="contact_info_text">оибт.рф</div>
+                    </div>
                 </div>
+                <div class="col-md-12">
+                    <h3 class="contact_form_h3">Задайте вопрос</h3>
+                    <p class="contact_form_p">Если у Вас остались вопросы, задавайте, специалист ответит
+                        наиболееудобным для Вас способом</p>
+                </div>
+                <div class="col-md-2"></div>
+                <div class="col-md-8">
+                    <form class="contact1">
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <input type="tel" class="form-control" id="inputEmail4"
+                                       placeholder="Введите номер телефона:">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <input type="email" class="form-control" id="inputPassword4"
+                                       placeholder="Введите E-mail:">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"
+                                      placeholder="Введите Ваш вопрос:"></textarea>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Отправить вопрос</button>
+                    </form>
+                </div>
+                <div class="col-md-2"></div>
             </div>
         </div>
+    </div>
+    <div class="maps">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d897.472037900764!2d39.844548707299815!3d57.65596170617328!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b291975ff085d7%3A0xae1db2706ff662ac!2z0L_RgC4g0J7QutGC0Y_QsdGA0Y8sIDg4LCDQr9GA0L7RgdC70LDQstC70YwsINCv0YDQvtGB0LvQsNCy0YHQutCw0Y8g0L7QsdC7Liwg0KDQvtGB0YHQuNGPLCAxNTAwMDM!5e0!3m2!1sru!2sua!4v1513153789573"
+                width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
     </div>
 <?php include_once "footer.php"; ?>
