4fc7b3a668d4798e039556469f05b050604aa665
ValentinGor <v.gorbatenko@mail.ru>
2017-12-20 17:12:32 +0200

    Add page confidential information

---
+++ confidential.php
@@ -0,0 +1,58 @@
+<?php include_once "header.php"; ?>
+    <div class="confidential">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Согласие на обработку персональных данных</h2>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aperiam consectetur eius
+                        eligendi hic incidunt nisi odit officia ut veritatis. Cupiditate dolorem ea error fuga magni
+                        officia optio rerum vero. A aut deleniti facilis harum illo incidunt nemo nobis quam quo
+                        temporibus. A ad amet architecto, asperiores aspernatur atque autem dicta dolorem doloremque
+                        dolores eaque id in incidunt magnam minima mollitia natus neque odit placeat provident quam quas
+                        quidem quisquam rem repudiandae sapiente suscipit tempora totam unde vero. At, quas.</p>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa nisi repellendus repudiandae
+                        similique? Amet beatae culpa labore perferendis reiciendis rem.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa nisi repellendus repudiandae
+                        similique? Amet beatae culpa labore perferendis reiciendis rem.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aperiam consectetur eius
+                        eligendi hic incidunt nisi odit officia ut veritatis. Cupiditate dolorem ea error fuga magni
+                        officia optio rerum vero. A aut deleniti facilis harum illo incidunt nemo nobis quam quo
+                        temporibus. A ad amet architecto, asperiores aspernatur atque autem dicta dolorem doloremque
+                        dolores eaque id in incidunt magnam minima mollitia natus neque odit placeat provident quam quas
+                        quidem quisquam rem repudiandae sapiente suscipit tempora totam unde vero. At, quas.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque fugiat itaque labore laboriosam
+                        pariatur quo repellat voluptate? Aliquam eaque eius esse natus nulla quam ratione totam velit.
+                        Accusamus ducimus fuga hic ipsam iste labore laudantium, minima molestias mollitia natus neque,
+                        nesciunt nihil, nisi possimus provident quis reprehenderit velit vero voluptatibus.</p>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa nisi repellendus repudiandae
+                        similique? Amet beatae culpa labore perferendis reiciendis rem.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa nisi repellendus repudiandae
+                        similique? Amet beatae culpa labore perferendis reiciendis rem.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque fugiat itaque labore laboriosam
+                        pariatur quo repellat voluptate? Aliquam eaque eius esse natus nulla quam ratione totam velit.
+                        Accusamus ducimus fuga hic ipsam iste labore laudantium, minima molestias mollitia natus neque,
+                        nesciunt nihil, nisi possimus provident quis reprehenderit velit vero voluptatibus.</p>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa nisi repellendus repudiandae
+                        similique? Amet beatae culpa labore perferendis reiciendis rem.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa nisi repellendus repudiandae
+                        similique? Amet beatae culpa labore perferendis reiciendis rem.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque fugiat itaque labore laboriosam
+                        pariatur quo repellat voluptate? Aliquam eaque eius esse natus nulla quam ratione totam velit.
+                        Accusamus ducimus fuga hic ipsam iste labore laudantium, minima molestias mollitia natus neque,
+                        nesciunt nihil, nisi possimus provident quis reprehenderit velit vero voluptatibus.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aperiam consectetur eius
+                        eligendi hic incidunt nisi odit officia ut veritatis. Cupiditate dolorem ea error fuga magni
+                        officia optio rerum vero. A aut deleniti facilis harum illo incidunt nemo nobis quam quo
+                        temporibus. A ad amet architecto, asperiores aspernatur atque autem dicta dolorem doloremque
+                        dolores eaque id in incidunt magnam minima mollitia natus neque odit placeat provident quam quas
+                        quidem quisquam rem repudiandae sapiente suscipit tempora totam unde vero. At, quas.</p>
+
+                </div>
+            </div>
+        </div>
+    </div>
+<?php include_once "footer.php"; ?>
